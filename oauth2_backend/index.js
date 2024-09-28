9bcbcbba0642a0c5429a2263f75868da8e96d556
Lucas Moyano <lucasmoyano11@outlook.it>
2024-09-28 14:33:56 -0300

    setup env variables

--- oauth2_backend/index.js
+++ oauth2_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
-const port = 8000
+const port = process.env.PORT
 const cors = require('cors')
 
 // Enables cors, this is makes the backend work with nginx frontend
@@ -12,4 +13,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(process.env.PORT)
 })
