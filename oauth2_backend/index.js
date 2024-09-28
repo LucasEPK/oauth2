483064e6b5adfe8c8dd24182f81d8070b727626a
Lucas Moyano <lucasmoyano11@outlook.it>
2024-09-28 15:52:37 -0300

    backend now retrieves access token and user info

--- oauth2_backend/index.js
+++ oauth2_backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 const cors = require('cors')
+const { request } = require('undici');
 
 // Enables cors, this is makes the backend work with nginx frontend
 app.use(cors());
@@ -11,12 +12,53 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/discord', (req, res) => {
-  console.log(req.query.code)
-  res.send(req.query.code)
+// This is called from the discord redirect url
+app.get('/discord', async ({ query }, res) => {
+
+  // Here we get access code
+  const { code } = query;
+
+  let userResult
+  
+  // Here we get access token and request user data
+  if (code) {
+    try {
+      // Getting acess token
+      const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+        method: 'POST',
+        body: new URLSearchParams({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code,
+          grant_type: 'authorization_code',
+          redirect_uri: `http://localhost:${port}/discord`,
+          scope: 'identify',
+        }).toString(),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+
+      const oauthData = await tokenResponseData.body.json();
+      console.log(oauthData)
+
+      // Requesting user data
+      userResult = await request('https://discord.com/api/users/@me', {
+        headers: {
+          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+        },
+      });
+    } catch (error) {
+      // NOTE: An unauthorized token will not throw an error
+      console.error(error);
+    }
+  }
+  
+  const data = await userResult.body.json()
+  await console.log(data['username']);
+  await res.send(data)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log(process.env.PORT)
 })
